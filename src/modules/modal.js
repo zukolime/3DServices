f61d4f907335d7d059c3f795c61c6e1240aecdbc
zukolime <alena.perminova.1993@gmail.com>
2025-02-28 15:00:11 +0500

    add: animation for modal

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,17 +2,56 @@ const modal = () => {
   const modal = document.querySelector(".popup");
   const popupBtn = document.querySelectorAll(".popup-btn");
   const closeBtn = document.querySelector(".popup-close");
+  const popupContent = document.querySelector(".popup-content");
 
-  console.log(closeBtn);
+  const isMobile = window.innerWidth <= 768;
+
+  let active = false;
+  let count = 0;
+  let idInterval;
+
+  const animateShowModal = () => {
+    count++;
+    idInterval = requestAnimationFrame(animateShowModal);
+
+    if (count <= 10) {
+      modal.style.display = `block`;
+      popupContent.style.top = `${count}%`;
+    } else {
+      cancelAnimationFrame(idInterval);
+      active = false;
+    }
+  };
+
+  const animateHideModal = () => {
+    count--;
+    idInterval = requestAnimationFrame(animateHideModal);
+
+    if (count >= 0) {
+      popupContent.style.top = `${count}%`;
+    } else {
+      cancelAnimationFrame(idInterval);
+      modal.style.display = `none`;
+      active = false;
+    }
+  };
 
   popupBtn.forEach((btn) => {
     btn.addEventListener("click", () => {
-      modal.style.display = `block`;
+      if (!isMobile) {
+        animateShowModal();
+      } else {
+        modal.style.display = `block`;
+      }
     });
   });
 
   closeBtn.addEventListener("click", () => {
-    modal.style.display = `none`;
+    if (!isMobile) {
+      animateHideModal();
+    } else {
+      modal.style.display = `none`;
+    }
   });
 };
 
