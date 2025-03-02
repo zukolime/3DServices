f2f4904602f77b7b843a56db791e22fa601eca23
zukolime <alena.perminova.1993@gmail.com>
2025-03-02 11:57:01 +0500

    fix: re-click smooth scroll

--- src/modules/smoothScroll.js
+++ src/modules/smoothScroll.js
@@ -1,16 +1,16 @@
 const smoothScroll = () => {
   const scrollBtn = document.querySelector("main>a");
-  const targetBlock = document.getElementById("service-block").offsetTop;
+  const targetBlock = document.getElementById("service-block");
 
-  let startPosition = document.documentElement.scrollTop;
   let idInterval;
 
   const animateSmoothScroll = () => {
-    startPosition += 24;
-    idInterval = requestAnimationFrame(animateSmoothScroll);
+    const startPosition = document.documentElement.scrollTop;
+    const step = 16;
 
-    if (startPosition < targetBlock) {
-      document.documentElement.scrollTop = `${startPosition}`;
+    if (startPosition < targetBlock.offsetTop) {
+      document.documentElement.scrollTop = startPosition + step;
+      idInterval = requestAnimationFrame(animateSmoothScroll);
     } else {
       cancelAnimationFrame(idInterval);
     }
